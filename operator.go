a6345bc7124c4ccf099e8debc57372753657dac6
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2019-09-21 01:32:08 +0900

    加算演算子が引数を再帰的に評価していなかった不具合を修正

--- operator.go
+++ operator.go
@@ -6,8 +6,6 @@ import (
 	"github.com/healthy-tiger/gostree"
 )
 
-type operatorAdd struct{}
-
 const (
 	addSymbol = "+"
 )
@@ -17,29 +15,36 @@ var (
 )
 
 // Eval オペラントの評価結果がすべてint64またはfloat64の値の場合にそれらの合計を返す。
-func (op *operatorAdd) Eval(lst *gostree.List, locals *Namespace, globals *Namespace) (interface{}, error) {
+func addBody(_ interface{}, lst *gostree.List, locals *Namespace, globals *Namespace) (interface{}, error) {
 	if lst.Len() < 2 {
 		return nil, errorInsufficientNumberOfArguments
 	}
-	result, err := EvalElement(lst.ElementAt(1), locals, globals)
-	if err != nil {
-		return nil, err
+	// 引数をすべて評価する。
+	params := make([]interface{}, lst.Len())
+	for i := 1; i < lst.Len(); i++ {
+		ev, err := EvalElement(lst.ElementAt(i), locals, globals)
+		if err != nil {
+			return nil, err
+		}
+		params[i] = ev
 	}
+
+	// 引数を最初の引数の型に合わせながらすべて加算する。
+	result := params[1]
 	for i := 2; i < lst.Len(); i++ {
-		e := lst.ElementAt(i)
 		switch v := result.(type) {
 		case int64:
-			if iv, ok := e.IntValue(); ok {
+			if iv, ok := params[i].(int64); ok {
 				result = v + iv
-			} else if fv, ok := e.FloatValue(); ok {
+			} else if fv, ok := params[i].(float64); ok {
 				result = v + int64(fv)
 			} else {
 				return nil, errorOperantIsATypeOfDataThatCannotBeAdded
 			}
 		case float64:
-			if iv, ok := e.IntValue(); ok {
+			if iv, ok := params[i].(int64); ok {
 				result = v + float64(iv)
-			} else if fv, ok := e.FloatValue(); ok {
+			} else if fv, ok := params[i].(float64); ok {
 				result = v + fv
 			} else {
 				return nil, errorOperantIsATypeOfDataThatCannotBeAdded
@@ -51,10 +56,10 @@ func (op *operatorAdd) Eval(lst *gostree.List, locals *Namespace, globals *Names
 	return result, nil
 }
 
-var addOperator operatorAdd
+var addOperator = Extension{nil, addBody}
 
 // RegisterOperators streeに演算子のシンボルを、nsに演算子に対応する拡張関数をそれぞれ登録する。
 func RegisterOperators(stree *gostree.STree, ns *Namespace) {
 	addid := stree.GetSymbolID(addSymbol)
-	ns.Set(addid, addOperator)
+	ns.Set(addid, &addOperator)
 }
