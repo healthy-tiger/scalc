37c7ea8ee37685a143afb55350645fccb0905207
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2019-09-23 10:58:55 +0900

    パースエラーと評価時エラーをテストできるようにした。また、除算テストの実行漏れを修正。

--- operator_test.go
+++ operator_test.go
@@ -2,85 +2,103 @@ package scalc
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 	"testing"
 
 	"github.com/healthy-tiger/gostree"
 )
 
 type optest struct {
-	src      string
-	expected interface{}
+	src        string
+	parseError bool // パースエラーが期待値ならtrue
+	evalError  bool // 評価時エラーが期待値ならtrue
+	expected   interface{}
 }
 
 var addtests []optest = []optest{
-	{`(+ 1 2 3)`, int64(1) + int64(2) + int64(3)},
-	{`(+ 1 2 (+ 10 20) 3)`, int64(1) + int64(2) + (int64(10) + int64(20)) + int64(3)},
-	{`(+ 1 2 (+ -10 -20) 3)`, int64(1) + int64(2) + (int64(-10) + int64(-20)) + int64(3)},
-	{`(+ 1.0 2.0 3.0)`, float64(1.0) + float64(2.0) + float64(3.0)},
-	{`(+ 1 2 (+ 10.0 20) 3)`, float64(int64(1)+int64(2)) + (float64(10.0) + float64(20.0)) + float64(int64(3))},
-	{`(+ 1 2 (+ -10.0 -20.0) 3)`, float64(int64(1)+int64(2)) + (float64(-10.0) + float64(-20.0)) + float64(int64(3))},
-	{`(+ 1.0 2 3)`, (float64(1.0) + float64(int64(2))) + float64(int64(3))},
-	{`(+ 1 2.1 3.0)`, float64(int64(1)) + float64(2.1) + float64(3.0)},
+	{`(+ 1 2 3)`, false, false, int64(1) + int64(2) + int64(3)},
+	{`(+ 1 2 (+ 10 20) 3)`, false, false, int64(1) + int64(2) + (int64(10) + int64(20)) + int64(3)},
+	{`(+ 1 2 (+ -10 -20) 3)`, false, false, int64(1) + int64(2) + (int64(-10) + int64(-20)) + int64(3)},
+	{`(+ 1.0 2.0 3.0)`, false, false, float64(1.0) + float64(2.0) + float64(3.0)},
+	{`(+ 1 2 (+ 10.0 20) 3)`, false, false, float64(int64(1)+int64(2)) + (float64(10.0) + float64(20.0)) + float64(int64(3))},
+	{`(+ 1 2 (+ -10.0 -20.0) 3)`, false, false, float64(int64(1)+int64(2)) + (float64(-10.0) + float64(-20.0)) + float64(int64(3))},
+	{`(+ 1.0 2 3)`, false, false, (float64(1.0) + float64(int64(2))) + float64(int64(3))},
+	{`(+ 1 2.1 3.0)`, false, false, float64(int64(1)) + float64(2.1) + float64(3.0)},
+	{`(+ "abc" "123")`, false, false, "abc123"},
+	{`(+ "abc" 123)`, false, false, "abc123"},
+	{`(+ "abc" 1.0)`, false, false, "abc" + strconv.FormatFloat(1.0, 'e', -1, 64)},
+	//{`(+ 1 + "abc")`, false, false, nil}, // 算術データ型が必要な胸のエラーがでるはず
+	{`(+ "" 123)`, false, false, "123"},
 }
 
 var subtests []optest = []optest{
-	{`(- 1 2 3)`, int64(1) - int64(2) - int64(3)},
-	{`(- 1 2 (- 10 20) 3)`, int64(1) - int64(2) - (int64(10) - int64(20)) - int64(3)},
-	{`(- 1 2 (- -10 -20) 3)`, int64(1) - int64(2) - (int64(-10) - int64(-20)) - int64(3)},
-	{`(- 1.0 2.0 3.0)`, float64(1.0) - float64(2.0) - float64(3.0)},
-	{`(- 1 2 (- 10.0 20) 3)`, float64(int64(1)-int64(2)) - (float64(10.0) - float64(20.0)) - float64(int64(3))},
-	{`(- 1 2 (- -10.0 -20.0) 3)`, float64(int64(1)-int64(2)) - (float64(-10.0) - float64(-20.0)) - float64(int64(3))},
-	{`(- 1.0 2 3)`, (float64(1.0) - float64(int64(2))) - float64(int64(3))},
-	{`(- 1 2.1 3.0)`, float64(int64(1)) - float64(2.1) - float64(3.0)},
+	{`(- 1 2 3)`, false, false, int64(1) - int64(2) - int64(3)},
+	{`(- 1 2 (- 10 20) 3)`, false, false, int64(1) - int64(2) - (int64(10) - int64(20)) - int64(3)},
+	{`(- 1 2 (- -10 -20) 3)`, false, false, int64(1) - int64(2) - (int64(-10) - int64(-20)) - int64(3)},
+	{`(- 1.0 2.0 3.0)`, false, false, float64(1.0) - float64(2.0) - float64(3.0)},
+	{`(- 1 2 (- 10.0 20) 3)`, false, false, float64(int64(1)-int64(2)) - (float64(10.0) - float64(20.0)) - float64(int64(3))},
+	{`(- 1 2 (- -10.0 -20.0) 3)`, false, false, float64(int64(1)-int64(2)) - (float64(-10.0) - float64(-20.0)) - float64(int64(3))},
+	{`(- 1.0 2 3)`, false, false, (float64(1.0) - float64(int64(2))) - float64(int64(3))},
+	{`(- 1 2.1 3.0)`, false, false, float64(int64(1)) - float64(2.1) - float64(3.0)},
 }
 
 var multests []optest = []optest{
-	{`(* 1 2 3)`, int64(1) * int64(2) * int64(3)},
-	{`(* 1 2 (* 10 20) 3)`, int64(1) * int64(2) * (int64(10) * int64(20)) * int64(3)},
-	{`(* 1 2 (* -10 -20) 3)`, int64(1) * int64(2) * (int64(-10) * int64(-20)) * int64(3)},
-	{`(* 1.0 2.0 3.0)`, float64(1.0) * float64(2.0) * float64(3.0)},
-	{`(* 1 2 (* 10.0 20) 3)`, float64(int64(1)*int64(2)) * (float64(10.0) * float64(20.0)) * float64(int64(3))},
-	{`(* 1 2 (* -10.0 -20.0) 3)`, float64(int64(1)*int64(2)) * (float64(-10.0) * float64(-20.0)) * float64(int64(3))},
-	{`(* 1.0 2 3)`, (float64(1.0) * float64(int64(2))) * float64(int64(3))},
-	{`(* 1 2.1 3.0)`, float64(int64(1)) * float64(2.1) * float64(3.0)},
+	{`(* 1 2 3)`, false, false, int64(1) * int64(2) * int64(3)},
+	{`(* 1 2 (* 10 20) 3)`, false, false, int64(1) * int64(2) * (int64(10) * int64(20)) * int64(3)},
+	{`(* 1 2 (* -10 -20) 3)`, false, false, int64(1) * int64(2) * (int64(-10) * int64(-20)) * int64(3)},
+	{`(* 1.0 2.0 3.0)`, false, false, float64(1.0) * float64(2.0) * float64(3.0)},
+	{`(* 1 2 (* 10.0 20) 3)`, false, false, float64(int64(1)*int64(2)) * (float64(10.0) * float64(20.0)) * float64(int64(3))},
+	{`(* 1 2 (* -10.0 -20.0) 3)`, false, false, float64(int64(1)*int64(2)) * (float64(-10.0) * float64(-20.0)) * float64(int64(3))},
+	{`(* 1.0 2 3)`, false, false, (float64(1.0) * float64(int64(2))) * float64(int64(3))},
+	{`(* 1 2.1 3.0)`, false, false, float64(int64(1)) * float64(2.1) * float64(3.0)},
 }
 
 var divtests []optest = []optest{
-	{`(/ 1 2 3)`, int64(1) / int64(2) / int64(3)},
-	{`(/ 1 2 (/ 10 20) 3)`, nil},
-	{`(/ 1 2 (/ -10 -20) 3)`, nil},
-	{`(/ 1.0 2.0 3.0)`, float64(1.0) / float64(2.0) / float64(3.0)},
-	{`(/ 1 2 (/ 10.0 20) 3)`, float64(int64(1)/int64(2)) / (float64(10.0) / float64(20.0)) / float64(int64(3))},
-	{`(/ 1 2 (/ -10.0 -20.0) 3)`, float64(int64(1)/int64(2)) / (float64(-10.0) / float64(-20.0)) / float64(int64(3))},
-	{`(/ 1.0 2 3)`, (float64(1.0) / float64(int64(2))) / float64(int64(3))},
-	{`(/ 1 2.1 3.0)`, float64(int64(1)) / float64(2.1) / float64(3.0)},
+	{`(/ 1 2 3)`, false, false, int64(1) / int64(2) / int64(3)},
+	{`(/ 1 0)`, false, true, nil},
+	{`(/ 1 2 (/ 10 20) 3)`, false, true, nil},
+	{`(/ 1 2 (/ -10 -20) 3)`, false, true, nil},
+	{`(/ 1.0 2.0 3.0)`, false, false, float64(1.0) / float64(2.0) / float64(3.0)},
+	{`(/ 1 2 (/ 10.0 20) 3)`, false, false, float64(int64(1)/int64(2)) / (float64(10.0) / float64(20.0)) / float64(int64(3))},
+	{`(/ 1 2 (/ -10.0 -20.0) 3)`, false, false, float64(int64(1)/int64(2)) / (float64(-10.0) / float64(-20.0)) / float64(int64(3))},
+	{`(/ 1.0 2 3)`, false, false, (float64(1.0) / float64(int64(2))) / float64(int64(3))},
+	{`(/ 1 2.1 3.0)`, false, false, float64(int64(1)) / float64(2.1) / float64(3.0)},
+	{`(/ 1 0.0)`, false, false, math.Inf(1)},
 }
 
 func doOpTests(name string, t *testing.T, tests []optest) {
 	for i, tst := range tests {
 		st, err := gostree.ParseString(fmt.Sprintf("%v%d", name, i), tst.src)
 		if err != nil {
-			t.Errorf("[%d]Parse error: %v\n", i, err)
+			if !tst.parseError {
+				t.Errorf("[%d]Parse error: %v\n", i, err)
+			}
 		} else {
 			ns := DefaultNamespace(st)
 			result, err := EvalList(st.Lists[0], ns)
 			if err != nil {
-				t.Fatalf("[%d]Eval error: %v\n", i, err)
-			}
-			success := false
-			if ir, ok := result.(int64); ok {
-				if ie, ok := tst.expected.(int64); ok && ir == ie {
-					success = true
+				if !tst.evalError {
+					t.Errorf("[%d]Eval error: %v\n", i, err)
 				}
-			} else if fr, ok := result.(float64); ok {
-				if fe, ok := tst.expected.(float64); ok && fr == fe {
-					success = true
+			} else {
+				success := false
+				if ir, ok := result.(int64); ok {
+					if ie, ok := tst.expected.(int64); ok && ir == ie {
+						success = true
+					}
+				} else if fr, ok := result.(float64); ok {
+					if fe, ok := tst.expected.(float64); ok && fr == fe {
+						success = true
+					}
+				} else if sr, ok := result.(string); ok {
+					if se, ok := tst.expected.(string); ok && sr == se {
+						success = true
+					}
 				}
-			} else if result == nil && tst.expected == nil {
-				success = true
-			}
-			if !success {
-				t.Errorf("[%d]The expected value was %v, but the result was %v.", i, tst.expected, result)
+				if !success {
+					t.Errorf("[%d]The expected value was %v, but the result was %v.", i, tst.expected, result)
+				}
 			}
 		}
 	}
@@ -96,4 +114,8 @@ func TestSub(t *testing.T) {
 
 func TestMul(t *testing.T) {
 	doOpTests("TestMul", t, multests)
+}
+
+func TestDiv(t *testing.T) {
+	doOpTests("TestDiv", t, divtests)
 }
