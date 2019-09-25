5be99a3a5e8a7c32f59efb20f62a5ca56345f4ce
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2019-09-26 00:18:10 +0900

    簡易的なREPLをmainパッケージに実装

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/healthy-tiger/scalc
+
+go 1.13
+
+require github.com/healthy-tiger/scalc/parser v0.0.0-20190924154012-e06b6f165f0c
+
+require github.com/healthy-tiger/scalc/runtime v0.0.0-20190924154012-e06b6f165f0c
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/healthy-tiger/scalc/parser"
+	"github.com/healthy-tiger/scalc/runtime"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	st := parser.NewSymbolTable()
+	ns := runtime.DefaultNamespace(st)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lists, err := parser.ParseString("stdin", st, line)
+		if err != nil && err != io.EOF {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		} else {
+			for i := 0; i < len(lists); i++ {
+				result, err := runtime.EvalList(lists[i], ns)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error: %v\n", err)
+				} else {
+					fmt.Printf("result: %v\n", result)
+				}
+			}
+		}
+	}
+}
