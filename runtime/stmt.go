7fd2e61626fc9f2cf52c1f80847ab11b21ce2b3f
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2019-10-03 23:52:06 +0900

    while文を追加

--- runtime/stmt.go
+++ runtime/stmt.go
@@ -9,6 +9,7 @@ import (
 const (
 	setSymbol   = "set"
 	ifSymbol    = "if"
+	whileSymbol = "while"
 	printSymbol = "print"
 )
 
@@ -62,6 +63,41 @@ func ifBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error)
 	return nil, newEvalError(lst.ElementAt(1).Position(), ErrorOperantsMustBeBoolean)
 }
 
+func evalAsBool(elm parser.SyntaxElement, ns *Namespace) (bool, error) {
+	r, err := EvalElement(elm, ns)
+	if err != nil {
+		return false, err
+	}
+	c, ok := r.(bool)
+	if ok {
+		return c, nil
+	}
+	return false, newEvalError(elm.Position(), ErrorOperantsMustBeBoolean)
+}
+
+func whileBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() < 3 {
+		return nil, newEvalError(lst.Position(), ErrorInsufficientNumberOfArguments)
+	} else if lst.Len() > 3 {
+		return nil, newEvalError(lst.Position(), ErrorTooManyArguments)
+	}
+
+	condelm := lst.ElementAt(1)
+	bodyelm := lst.ElementAt(2)
+	cond, err := evalAsBool(condelm, ns)
+	var result interface{} = nil
+	for err == nil && cond {
+		result, err = EvalElement(bodyelm, ns)
+		if err == nil { // bodyelmを評価してエラーがなければ再度、ループの条件を確認する。
+			cond, err = evalAsBool(condelm, ns)
+		}
+	}
+	if err != nil { // エラーで抜けた場合はEvalError
+		return nil, err
+	}
+	return result, nil
+}
+
 func printBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
 	params := make([]interface{}, lst.Len()-1)
 	for i := 1; i < lst.Len(); i++ {
@@ -79,4 +115,5 @@ func RegisterStmt(st *parser.SymbolTable, ns *Namespace) {
 	RegisterExtension(st, ns, setSymbol, nil, setBody)
 	RegisterExtension(st, ns, ifSymbol, nil, ifBody)
 	RegisterExtension(st, ns, printSymbol, nil, printBody)
+	RegisterExtension(st, ns, whileSymbol, nil, whileBody)
 }
