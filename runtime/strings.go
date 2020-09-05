f56105ad280eeff13ab1be5518757150864fc044
healthy-tiger <12779326+healthy-tiger@users.noreply.github.com>
2020-09-06 01:22:35 +0900

    文字列操作関数を追加（まだ全部じゃない）

--- runtime/strings.go
+++ runtime/strings.go
@@ -7,9 +7,20 @@ import (
 )
 
 const (
-	strCmpSymbol        = "strcmp"
-	strCmpNaturalSymbol = "strcmp-natural"
-	containsSymbol      = "contains"
+	strCmpSymbol        = "str-cmp"
+	strCmpNaturalSymbol = "str-cmp-natural"
+	containsSymbol      = "str-contains"
+	containsAnySymbol   = "str-contains-any"
+	countSymbol         = "str-count"
+	equalFoldSymbol     = "str-equal-fold"
+	hasPrefixSymbol     = "str-has-prefix"
+	hasSuffixSymbol     = "str-has-suffix"
+	indexSymbol         = "str-index"
+	indexAnySymbol      = "str-index-any"
+	lastIndexSymbol     = "str-lastindex"
+	lastIndexAnySymbol  = "str-lastindex-any"
+	repeatSymbol        = "str-repeat"
+	replaceSymbol       = "str-replace"
 )
 
 func toNaturalString(s string) []rune {
@@ -111,9 +122,228 @@ func containsBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, 
 	return BoolToInt(strings.Contains(a, b)), nil
 }
 
+func containsAnyBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsString(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	return BoolToInt(strings.ContainsAny(a, b)), nil
+}
+
+func countBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsString(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	return int64(strings.Count(a, b)), nil
+}
+
+func equalFoldBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsString(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	return BoolToInt(strings.EqualFold(a, b)), nil
+}
+
+func hasPrefixBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsString(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	return BoolToInt(strings.HasPrefix(a, b)), nil
+}
+
+func hasSuffixBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsString(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	return BoolToInt(strings.HasSuffix(a, b)), nil
+}
+
+func indexBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsString(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	i := strings.Index(a, b)
+	if i >= 0 {
+		ss := a[:i]
+		return int64(len(ss)), nil
+	}
+	return int64(-1), nil
+}
+
+func indexAnyBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsString(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	i := strings.IndexAny(a, b)
+	if i >= 0 {
+		ss := a[:i]
+		return int64(len(ss)), nil
+	}
+	return int64(-1), nil
+}
+
+func lastIndexBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsString(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	i := strings.LastIndex(a, b)
+	if i >= 0 {
+		ss := a[:i]
+		return int64(len(ss)), nil
+	}
+	return int64(-1), nil
+}
+
+func lastIndexAnyBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsString(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	i := strings.LastIndexAny(a, b)
+	if i >= 0 {
+		ss := a[:i]
+		return int64(len(ss)), nil
+	}
+	return int64(-1), nil
+}
+
+func repeatBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() != 3 {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+	a, aerr := EvalAsString(lst.ElementAt(1), ns)
+	b, berr := EvalAsInt(lst.ElementAt(2), ns)
+	if aerr != nil {
+		return nil, aerr
+	}
+	if berr != nil {
+		return nil, berr
+	}
+	return strings.Repeat(a, int(b)), nil
+}
+
+func replaceBody(_ interface{}, lst *parser.List, ns *Namespace) (interface{}, error) {
+	if lst.Len() == 4 {
+		a, aerr := EvalAsString(lst.ElementAt(1), ns)
+		b, berr := EvalAsString(lst.ElementAt(2), ns)
+		c, cerr := EvalAsString(lst.ElementAt(3), ns)
+		if aerr != nil {
+			return nil, aerr
+		}
+		if berr != nil {
+			return nil, berr
+		}
+		if cerr != nil {
+			return nil, cerr
+		}
+		return strings.ReplaceAll(a, b, c), nil
+	} else if lst.Len() == 5 {
+		a, aerr := EvalAsString(lst.ElementAt(1), ns)
+		b, berr := EvalAsString(lst.ElementAt(2), ns)
+		c, cerr := EvalAsString(lst.ElementAt(3), ns)
+		d, derr := EvalAsInt(lst.ElementAt(4), ns)
+		if aerr != nil {
+			return nil, aerr
+		}
+		if berr != nil {
+			return nil, berr
+		}
+		if cerr != nil {
+			return nil, cerr
+		}
+		if derr != nil {
+			return nil, derr
+		}
+		return strings.Replace(a, b, c, int(d)), nil
+	} else {
+		return nil, NewEvalError(lst.Position(), ErrorTheNumberOfArgumentsDoesNotMatch, lst.Len()-1, 2)
+	}
+}
+
 // RegisterStrings stに演算子のシンボルを、nsに演算子に対応する拡張関数をそれぞれ登録する。
 func RegisterStrings(ns *Namespace) {
 	ns.RegisterExtension(strCmpSymbol, nil, strCmpBody)
 	ns.RegisterExtension(strCmpNaturalSymbol, nil, strCmpNaturalBody)
 	ns.RegisterExtension(containsSymbol, nil, containsBody)
+	ns.RegisterExtension(containsAnySymbol, nil, containsAnyBody)
+	ns.RegisterExtension(countSymbol, nil, countBody)
+	ns.RegisterExtension(equalFoldSymbol, nil, equalFoldBody)
+	ns.RegisterExtension(hasPrefixSymbol, nil, hasPrefixBody)
+	ns.RegisterExtension(hasSuffixSymbol, nil, hasSuffixBody)
+	ns.RegisterExtension(indexSymbol, nil, indexBody)
+	ns.RegisterExtension(indexAnySymbol, nil, indexAnyBody)
+	ns.RegisterExtension(lastIndexSymbol, nil, lastIndexBody)
+	ns.RegisterExtension(lastIndexAnySymbol, nil, lastIndexAnyBody)
+	ns.RegisterExtension(repeatSymbol, nil, repeatBody)
+	ns.RegisterExtension(replaceSymbol, nil, replaceBody)
 }
